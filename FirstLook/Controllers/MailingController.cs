93948f9ed3cf4a6cedc4338fb55d5d292a4cdecd
brndnhghs <brndnhghs@gmail.com>
2018-01-29 15:04:15 -0800

    FirstLook
    ITC172 FirstLook in class assignment

---
+++ FirstLook/Controllers/MailingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+using FirstLook.Models;
+
+// can use Firstlook.Models.MailingList
+
+
+
+namespace FirstLook.Controllers
+{
+    public class MailingController : Controller
+    {
+        // GET: Mailing
+        public ActionResult Index()
+        {
+            MailingList m1 = new MailingList();
+            m1.FirstName = "Brandon";
+            m1.LastName = "Hughes";
+            m1.Email = "brndnhghs@gmail.com";
+
+            //TO HARDCODE IS not how you usually do it (usually from database)
+
+            MailingList m2 = new MailingList();
+            m2.FirstName = "Tandy";
+            m2.LastName = "Fugs";
+            m2.Email = "Tandy@gmail.com";
+
+            MailingList m3 = new MailingList();
+            m3.FirstName = "Blinklan";
+            m3.LastName = "Habble";
+            m3.Email = "BlinkHab@gmail.com";
+
+            List<MailingList> mailingObjectList = new List<MailingList>();
+
+            mailingObjectList.Add(m1);
+            mailingObjectList.Add(m2);
+            mailingObjectList.Add(m3);
+
+            return View(mailingObjectList);
+        }
+    }
+}
---
+++ FirstLook/Models/MailingList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstLook.Models
+{
+    public class MailingList
+    {
+
+        /* same as
+         * 
+         * public Mailing(string first, string last, string email) {
+         *  FirstName = first;
+         * }
+         * 
+         * Data classes typically dont have constructers but they can have them
+         * 
+         */
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+}
